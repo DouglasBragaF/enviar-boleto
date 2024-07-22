9c051e7b79eff864f2146067553ccb3051403921
Douglas França <douglasbragaf@gmail.com>
2024-07-22 20:53:23 -0300

    init project

---
+++ bolete/src/main/java/com/bolete/BoleteApplication.java
@@ -0,0 +1,13 @@
+package com.bolete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoleteApplication.class, args);
+	}
+
+}
---
+++ bolete/src/test/java/com/bolete/BoleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
